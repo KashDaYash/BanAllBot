3933372ca36a493cb69770dffce530b27481ad70
KashDaYash <85399597+KashDaYash@users.noreply.github.com>
2025-07-31 21:34:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-API_ID = os.environ.get('API_ID')
+API_ID = int(os.environ.get('API_ID'))
 API_HASH = os.environ.get('API_HASH')
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
-LOGGER_ID = os.environ.get('LOGGER_ID')
+LOGGER_ID = int(os.environ.get('LOGGER_ID'))
